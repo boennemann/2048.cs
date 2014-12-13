a97691c4b6c872f81ee4de38fe9eea677c970d7e
Stephan Bönnemann <stephan@boennemann.me>
2014-12-13 04:55:48 +0100

    feat(Cell): base class + empty, bonus and block decorators

---
+++ Cell.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _2048 {
+  public enum CellAction {
+    Block,
+    Merge,
+    Swap
+  }
+
+  public class Cell {
+    public int value;
+    private Boolean spawning = true;
+
+    public Cell() {
+      this.value = 2;
+    }
+
+    public Cell(int value) {
+      this.value = value;
+    }
+
+    public virtual CellAction AddressedBy(Cell other) {
+      if (this.value == other.value) {
+        return CellAction.Merge;
+      }
+      return CellAction.Block;
+    }
+
+    public override string ToString() {
+      var output = "";
+      if (this.spawning) {
+        output += "N";
+        this.spawning = false;
+      }
+      return output + this.value.ToString();
+    }
+  }
+
+  public class EmptyCell : Cell {
+    public EmptyCell() : base(0) {}
+
+    public override CellAction AddressedBy(Cell other) {
+      return CellAction.Swap;
+    }
+
+    public override string ToString() {
+      return "";
+    }
+  }
+
+  public class BonusCell : EmptyCell {
+    public override CellAction AddressedBy(Cell other) {
+      // emit event for bonus points
+      return base.AddressedBy(other);
+    }
+
+    public override string ToString() {
+      return "B";
+    }
+  }
+
+  public class BlockCell : EmptyCell {
+    public override CellAction AddressedBy(Cell other) {
+      return CellAction.Block;
+    }
+
+    public override string ToString() {
+      return "X";
+    }
+  }
+}
+
