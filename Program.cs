0967d877c6f0ebe648f6cb307ad079dba9a75f7f
Stephan Bönnemann <stephan@boennemann.me>
2014-12-13 05:18:44 +0100

    feat(Program): main method w/input handling

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using _2048;
+
+class MainClass {
+  public static void Main(string[] args) {
+    var game = new Game(4, 4);
+
+    game.Move();
+
+    while(true) {
+      switch (Console.ReadKey().Key) {
+      case ConsoleKey.W:
+      case ConsoleKey.UpArrow:
+        game.Move(Direction.Up);
+        break;
+      case ConsoleKey.S:
+      case ConsoleKey.DownArrow:
+        game.Move(Direction.Down);
+        break;
+      case ConsoleKey.A:
+      case ConsoleKey.LeftArrow:
+        game.Move(Direction.Left);
+        break;
+      case ConsoleKey.D:
+      case ConsoleKey.RightArrow:
+        game.Move(Direction.Right);
+        break;
+      case ConsoleKey.Escape:
+        Environment.Exit(0);
+        break;
+      }
+    }
+  }
+}
